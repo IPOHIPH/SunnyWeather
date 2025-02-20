23b4e2e91510677b78c35ec4edbddccd12ff9606
lixueosng <1748367307@qq.com>
2025-02-20 16:29:56 +0800

    Add city switching and manual weather update functionality

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.sunnyweather.android.R
@@ -27,16 +28,26 @@ class PlaceAdapter(private val fragment : PlaceFragment, private val placeList :
         holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = placeList[position]
-            val intent = Intent(parent.context , WeatherActivity::class.java).apply {
-                putExtra("location_lng" , place.location.lng)
-                putExtra("location_lat" , place.location.lat)
-                putExtra("place_name" , place.name)
+
+            val activity = fragment.activity
+            if (activity is WeatherActivity){
+                activity.binding.drawerLayout.closeDrawers()
+                activity.viewModel.placeName = place.name
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.locationLng = place.location.lng
+                activity.refreshWeather()
+            }
+            else {
+                val intent = Intent(parent.context , WeatherActivity::class.java).apply {
+                    putExtra("location_lng" , place.location.lng)
+                    putExtra("location_lat" , place.location.lat)
+                    putExtra("place_name" , place.name)
+                }
+                fragment.startActivity(intent)
+                activity?.finish()
             }
             fragment.viewModel.savePlace(place)
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
         }
-
         return holder
     }
 
